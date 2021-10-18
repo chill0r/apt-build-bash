4db5198bff2d832a55def45350173ead3544e71d
chill0r <1339474+chill0r@users.noreply.github.com>
2021-10-18 15:55:41 +0200

    Changed dependendy handling

--- handlers.sh
+++ handlers.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-all_deps=""
 
 function handler_install {
 	# Handles building and installations
@@ -11,18 +10,9 @@ function handler_install {
 		return
 	fi
 	
-	echo "[ ----- Getting dependencies for $todo -----]"
-	get_build_dependencies $todo || exit 1
-	local deps=$ret
+
 	
-	if [[ $recursive == true ]]; then
-		# Recursive call of dependency building requested
-		handler_install true $deps
-	else
-		install_package true $deps || exit 1
-		all_deps=$deps
-	fi
-	
+
 	# Deepest level of dependencies or building the real packages now
 	for x in $todo; do
 		# Check if the package is in the ignored packages list
@@ -37,6 +27,15 @@ function handler_install {
 			debug 0 "Package $x marked as ignored. Skipping"
 			continue
 		fi
+		echo "[ ----- Getting dependencies for $todo -----]"
+		get_build_dependencies $todo || exit 1
+		local deps=$ret
+		if [[ $recursive == true ]]; then
+			# Recursive call of dependency building requested
+			handler_install true $deps
+		else
+			install_package true $deps || exit 1
+		fi
 		get_source_package $x
 		local src_pkg=$ret
 		echo "[ ----- Downloading source for $x ----- ]"
@@ -49,21 +48,16 @@ function handler_install {
 		build_package $pkgdir $pkg_cache $src_pkg
 		get_version $pkgdir
 		local vers=$ret
+		# Remove the dependencies
+		apt-get $apt_yes --purge remove $deps
 		# Wait a bit for local-apt-repository to find / index our file
 		sleep 2
 		apt-get update # Update to get our new version
-		echo "[ ----- Installing $x:$vers -----]"		
+		echo "[ ----- Installing $x:$vers -----]"
+		# Note: If a removed build dependency is a runtime dependency too it'll be installed
+		# again so I don't need to care about that
 		install_package $is_dep "$x=$vers" || exit 1 # Install exactly our version
-		if [[ $is_dep == true ]]; then
-			#Keep track of the dependencies
-			all_deps+=" $x"
-		fi
 	done
-	
-	if [[ $is_dep != true ]]; then
-		# Remoe the dependencies since we're finished 
-		apt-get $apt_yes --purge remove $all_deps
-	fi
 	
 }
 
